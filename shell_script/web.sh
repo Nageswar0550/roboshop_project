bd4d927b22d0bd85f66c43186fb9b38bae0df87b
nageswar0550 <nageswar5050@gmail.com>
2024-05-28 00:58:16 +0530

    Modifying small changes

--- shell_script/web.sh
+++ shell_script/web.sh
@@ -67,7 +67,7 @@ if [ -d "/tmp/robot-shop/" ]
 then
     echo "$Y Frontend data already available $N"
 else
-    git clone https://github.com/instana/robot-shop.git /tmp/web/
-    cp -r /tmp/web/robot-shop/web/static/ /web/
+    git clone https://github.com/instana/robot-shop.git /tmp/web/ &>> $LOGFILE
+    cp -r /tmp/web/robot-shop/web/static/ /web/ &>> $LOGFILE
     echo "$G Frontend data copying $N"
 fi
