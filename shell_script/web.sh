6d69512b953032c585e939f55eea22224e6a60ca
nageswar0550 <nageswar5050@gmail.com>
2024-05-27 23:24:42 +0530

    Modifying small changes

--- shell_script/web.sh
+++ shell_script/web.sh
@@ -5,15 +5,17 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGFILE=/var/log/shell_script/$0-$TIMESTAMP.log
 TIMESTAMP=$(date +%F-%T)
+
+LOGFILE="/root/log/shell_script/$0-$TIMESTAMP.log"
+
 ID=$(id -u)
 
 VALIDATE () {
     if [ $? -ne 0 ]
     then
         echo  " $1...$R FAILED $N"
-        exit 1
+        exit
     else
         echo " $1...$G SUCCESS $N"
     fi
@@ -22,7 +24,7 @@ VALIDATE () {
 if [ $ID -ne 0 ]
 then
     echo "$R You are not a root user, login as root $N"
-    exit 1
+    exit
 else
     echo "$G You are root user and executing script $N"
     echo "Script executing at $TIMESTAMP" &>> $LOGFILE
@@ -37,7 +39,7 @@ apt list --installed 2>/dev/null | grep nginx &>> $LOGFILE
 if [ $? - ne 0 ]
 then
     echo "$Y Nginx is not available, installing $N"
-    apt install nginx -y &>>$LOGFILE
+    apt install nginx -y &>> $LOGFILE
 else
     echo "$G Nginx is already installed $N"
 fi
