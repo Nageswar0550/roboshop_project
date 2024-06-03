8ec70de77e158b084adb19c1d505b756fd0eb333
Challa Rao <challa.rao@kyndryl.com>
2024-06-04 02:39:38 +0530

    Small change

--- shell_script/roboshop.sh
+++ shell_script/roboshop.sh
@@ -2,7 +2,12 @@
 
 INSTANCE_NAME={"web","mongo","mysql","rabbitmq","catalogue","user","cart","shipping","payment"}
 
-for ( i in $INSTANCE_NAME[@] )
+for  i in "${INSTANCE_NAME[@]}"
 do
-    aws ec2 run-instances --image-id ami-04b70fa74e45c3917 --count 9 --instance-type t2.micro --security-group-ids sg-0e159184a7bd00fd0 --key-name Lenovo --tag-specifications ResourceType=instance,Tags=[{Key=Name,Value=$i}]
+    if $i -eq "web"
+    then
+        aws ec2 run-instances --image-id ami-04b70fa74e45c3917 --instance-type t2.micro --security-group-ids sg-0e159184a7bd00fd0 --key-name Lenovo --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PublicIpAddress' --output text
+    else
+        aws ec2 run-instances --image-id ami-04b70fa74e45c3917 --instance-type t2.micro --security-group-ids sg-0e159184a7bd00fd0 --key-name Lenovo --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
+    fi
 done
