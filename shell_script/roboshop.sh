ff527a88c37b3064c6399918dbe0c3212fb5bfb0
Challa Rao <challa.rao@kyndryl.com>
2024-06-04 02:42:24 +0530

    Small change

--- shell_script/roboshop.sh
+++ shell_script/roboshop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-INSTANCE_NAME=("web" "mongo" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment")
+INSTANCE_NAME=("web" "mongodb" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment")
 
 for  i in "${INSTANCE_NAME[@]}"
 do
-    if $i -eq "web"
+    if [ $i -eq "web" ]
     then
         aws ec2 run-instances --image-id ami-04b70fa74e45c3917 --instance-type t2.micro --security-group-ids sg-0e159184a7bd00fd0 --key-name Lenovo --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PublicIpAddress' --output text
     else
