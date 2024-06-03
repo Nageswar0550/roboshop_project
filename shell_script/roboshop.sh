3ec3f34f1d9e4caa8c36941fe954fbb12c4afc33
Challa Rao <challa.rao@kyndryl.com>
2024-06-04 02:51:05 +0530

    Small change

--- shell_script/roboshop.sh
+++ shell_script/roboshop.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
-INSTANCE_NAME=("web" "mongodb" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment")
+AMI=ami-0f3c7d07486cad139 #this keeps on changing
+SG_ID=sg-0e159184a7bd00fd0 #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z10424741G2H2DBEZUCEI # replace your zone ID
+DOMAIN_NAME="challa.cloud"
 
-for  i in "${INSTANCE_NAME[@]}"
+for i in "${INSTANCES[@]}"
 do
-    if [ $i -eq "web" ]
-    then
-        aws ec2 run-instances --image-id ami-04b70fa74e45c3917 --instance-type t2.micro --security-group-ids sg-0e159184a7bd00fd0 --key-name Lenovo --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PublicIpAddress' --output text
+    if [ $i == "web" ]
+    then  
+        IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PublicIpAddress' --output text)
     else
-        aws ec2 run-instances --image-id ami-04b70fa74e45c3917 --instance-type t2.micro --security-group-ids sg-0e159184a7bd00fd0 --key-name Lenovo --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
+        IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     fi
+    echo "$i"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
 done
